ec8c1c9db8415461c1de2ab20f644b189760a1ca
nishan <nishanpradhan500@gmail.com>
2025-06-23 07:11:39 +0545

    update model and repo

--- lib/features/shared/user_profile/models/user_profile_model.dart
+++ lib/features/shared/user_profile/models/user_profile_model.dart
@@ -1,1 +1,34 @@
-class UserProfileModel {}
+class UserProfileModel {
+  final int id;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String phone;
+  final String role;
+  final String avatar;
+  final DateTime createdAt;
+
+  UserProfileModel({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.phone,
+    required this.role,
+    required this.avatar,
+    required this.createdAt,
+  });
+
+  factory UserProfileModel.fromJson(Map<String, dynamic> json) {
+    return UserProfileModel(
+      id: json['id'],
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      phone: json['phone'],
+      role: json['role'],
+      avatar: json['avatar'],
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
+}
--- lib/features/shared/user_profile/repository/my_profile_repository.dart
+++ lib/features/shared/user_profile/repository/my_profile_repository.dart
@@ -6,22 +6,39 @@ import '../models/user_profile_model.dart';
 //##-------------------MY PROFILE REPOSITORY-------------------------##
 abstract interface class MyProfileRepository {
   //##-------------------GET USER PROFILE-------------------------##
-  FutureEither<String> getMe({required UserProfileModel userProfileModel});
+  FutureEither<UserProfileModel> getMe();
 }
 
+//##-------------------MY PROFILE REPOSITORY IMPLEMENTATION-------------------------##
 class MyProfileRepositoryImpl implements MyProfileRepository {
   final ApiService _apiService;
 
+  //##-------------------CONSTRUCTOR INJECTION-------------------------##
   MyProfileRepositoryImpl({required ApiService apiService})
     : _apiService = apiService;
 
   //##-------------------GET USER PROFILE-------------------------##
   @override
-  FutureEither<String> getMe({
-    required UserProfileModel userProfileModel,
-  }) async {
-    final response = await _apiService.get<String>('auth/me');
+  FutureEither<UserProfileModel> getMe() async {
+    //##-------------------API CALL TO GET CURRENT USER-------------------------##
+    final response = await _apiService.get<Map>('auth/me');
 
-    return response.fold((failure) => Left(failure), (data) => Right(data));
+    //##-------------------HANDLE RESPONSE USING EITHER-------------------------##
+    return response.fold(
+      //##-------------------FAILURE CASE-------------------------##
+      (failure) => Left(failure),
+      //##-------------------SUCCESS CASE-------------------------##
+      (data) {
+        //##-------------------EXTRACT USER OBJECT FROM NESTED JSON-------------------------##
+        final userJson = data['data']['user'];
+
+        //##-------------------PARSE JSON TO USER PROFILE MODEL-------------------------##
+        final user = UserProfileModel.fromJson(userJson);
+
+        //##-------------------RETURN SUCCESSFUL USER PROFILE MODEL-------------------------##
+        return Right(user);
+      },
+    );
   }
 }
+//##-------------------END OF MY PROFILE REPOSITORY IMPLEMENTATION-------------------------##
