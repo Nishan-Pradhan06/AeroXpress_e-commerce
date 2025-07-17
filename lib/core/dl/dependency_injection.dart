abbb51a218bd4553bf19c455118c070518a35568
nishan <nishanpradhan500@gmail.com>
2025-07-17 11:27:37 +0545

    remaed to orders

--- lib/core/dl/dependency_injection.dart
+++ lib/core/dl/dependency_injection.dart
@@ -1,8 +1,8 @@
 import 'package:deal_sell/core/network/api_services.dart';
 import 'package:deal_sell/core/network/dio_client.dart';
 import 'package:deal_sell/features/auth/cubit/logout_cubit.dart';
-import 'package:deal_sell/features/cutomers/all_orders/bloc/all_order_bloc.dart';
-import 'package:deal_sell/features/cutomers/all_orders/repository/all_orders_repository.dart';
+import 'package:deal_sell/features/cutomers/orders/bloc/all_order_bloc.dart';
+import 'package:deal_sell/features/cutomers/orders/repository/all_orders_repository.dart';
 import 'package:deal_sell/features/cutomers/cart/bloc/add_to_cart/add_to_cart_bloc.dart';
 import 'package:deal_sell/features/cutomers/cart/bloc/delete_cart/delete_cart_bloc.dart';
 import 'package:deal_sell/features/cutomers/cart/bloc/get_cart/get_cart_bloc.dart';
