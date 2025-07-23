0eeaca821a48c00a3752f043eb3b12995970872b
nishan <nishanpradhan500@gmail.com>
2025-07-23 12:16:10 +0545

    remains for verify

---
+++ lib/features/shared/payments/bloc/khalti_payment_verify/khalti_payment_verify_event.dart
@@ -0,0 +1,6 @@
+// part of 'khalti_payment_verify_bloc.dart';
+
+// @freezed
+// class KhaltiPaymentVerifyEvent with _$KhaltiPaymentVerifyEvent {
+//   const factory KhaltiPaymentVerifyEvent.verifyKhaltiPayment() = _VerifyKhaltiPayment;
+// }
---
+++ lib/features/shared/payments/bloc/khalti_payment_verify/khalti_payment_verify_state.dart
@@ -0,0 +1,3 @@
+// part of 'khalti_payment_verify_bloc.dart';
+
+// typedef KhaltiPaymentVerifyState = BaseState<String>;
